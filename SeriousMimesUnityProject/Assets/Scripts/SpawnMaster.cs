c0222ee9680dfcde3cda24a18f2c1fbf34ee4741
hulbertDelton <hulbert.delton@yahoo.com>
2017-01-22 03:11:34 -0500

    Fix Delton merge

---
+++ SeriousMimesUnityProject/Assets/Scripts/SpawnMaster.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMaster : MonoBehaviour {
+
+	public EnemySpawner spawnPoint1;
+	public EnemySpawner spawnPoint2;
+	public EnemySpawner spawnPoint3;
+
+	public float delay1;
+	public float delay2;
+	public float delay3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
